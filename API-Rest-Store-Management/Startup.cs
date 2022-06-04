eaa93455903a1fab6cefab0937ee79296ac63895
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-04 19:17:56 -0300

    chore: configure required packages

---
+++ API-Rest-Store-Management/Startup.cs
@@ -0,0 +1,125 @@
+﻿using API_Rest_Store_Management.Core;
+using API_Rest_Store_Management.Core.IRepository;
+using API_Rest_Store_Management.Core.Repository;
+using API_Rest_Store_Management.Core.Services;
+using API_Rest_Store_Management.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace API_Rest_Store_Management
+{
+    public class Startup : IStartup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            services.AddMemoryCache();
+
+            services.ConfigureRateLimiting();
+            services.AddHttpContextAccessor();
+
+            services.ConfigureHttpCacheHeaders();
+
+            services.AddAuthentication();
+            services.ConfigureIdentity();
+            services.ConfigureJWT(Configuration);
+
+            services.AddCors(o =>
+            {
+                o.AddPolicy("AllowAll", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+            services.ConfigureAutoMapper();
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IAuthManager, AuthManager>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Store", Version = "v1" });
+            });
+
+            services.AddControllers().AddNewtonsoftJson(op =>
+            op.SerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.ConfigureVersioning();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "Store API");
+            });
+
+            app.ConfigureExceptionHandler();
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");
+
+            app.UseResponseCaching();
+            app.UseHttpCacheHeaders();
+            //app.UseIpRateLimiting();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
+public interface IStartup
+{
+    IConfiguration Configuration { get; }
+    void Configure(WebApplication app, IWebHostEnvironment environment);
+    void ConfigureServices(IServiceCollection services);
+}
+
+public static class StartupExtensions
+{
+    public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder WebAppBuilder) where TStartup : IStartup
+    {
+        var startup = Activator.CreateInstance(typeof(TStartup), WebAppBuilder.Configuration) as IStartup;
+        if (startup == null) throw new ArgumentException("Classe Startup.cs inválida");
+        {
+            startup.ConfigureServices(WebAppBuilder.Services);
+
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+
+            return WebAppBuilder;
+        }
+    }
+}
+
