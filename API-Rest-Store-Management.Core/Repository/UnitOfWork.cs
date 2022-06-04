99129244a974e2a1fb679eba718eb248a7311f13
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-04 19:20:29 -0300

    chore: configure operations

---
+++ API-Rest-Store-Management.Core/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using API_Rest_Store_Management.Core.IRepository;
+using API_Rest_Store_Management.Data;
+
+namespace API_Rest_Store_Management.Core.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
