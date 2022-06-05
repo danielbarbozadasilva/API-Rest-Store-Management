561f97e269e2ad1705207ffc8403704a6be68ba8
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-05 14:58:27 -0300

    feat: add stock items schema

---
+++ API-Rest-Store-Management.Core/DTOs/StockItemDTO.cs
@@ -0,0 +1,24 @@
+﻿using API_Rest_Store_Management.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_Rest_Store_Management.Core.DTOs
+{
+    public class CreateStockItemDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int StoreId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+    }
+
+    public class StockDTO : CreateStockItemDTO
+    {
+        public int Id { get; set; }
+        public ICollection<StockItem> StockItem { get; set; }
+    }
+}
---
+++ API-Rest-Store-Management.Data/StockItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Rest_Store_Management.Data
+{
+    public class StockItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(Store))]
+        public int StoreId { get; set; }
+    }
+}
