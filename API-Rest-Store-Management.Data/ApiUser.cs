43bad23503fb3e32424a7f1371589acfbed84445
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-04 23:38:50 -0300

    chore: configure api user

---
+++ API-Rest-Store-Management.Data/ApiUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API_Rest_Store_Management.Data
+{
+    public class ApiUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
