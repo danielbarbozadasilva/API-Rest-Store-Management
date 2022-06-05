7bfd9741de88b942691188b4b75ee3973b77f21a
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-05 14:57:21 -0300

    feat: add products schema

---
+++ API-Rest-Store-Management.Core/DTOs/ProductDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Rest_Store_Management.Core.DTOs
+{
+    public class CreateProductDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Name Is Too Long")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Description Is Too Long")]
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public int StoreId { get; set; }
+    }
+
+    public class UpdateProductDTO : CreateProductDTO
+    {
+
+    }
+
+    public class ProductDTO : CreateProductDTO
+    {
+        public int Id { get; set; }
+        public StoreDTO Store { get; set; }
+    }
+}
---
+++ API-Rest-Store-Management.Data/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Rest_Store_Management.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        [ForeignKey(nameof(Store))]
+        public int StoreId { get; set; }
+        public ICollection<Store> Store { get; set; }
+    }
+}
