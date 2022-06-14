241618be6a403306fb5e482c15e544b1d4e05b2a
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-14 00:31:40 -0300

    feat: ensure delete store

--- API-Rest-Store-Management/Controller/StoreController.cs
+++ API-Rest-Store-Management/Controller/StoreController.cs
@@ -27,8 +27,8 @@ namespace API_Rest_Store_Management.Controller
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetStores([FromQuery] RequestParams requestParams)
         {
-            var products = await _unitOfWork.Stores.GetPagedList(requestParams);
-            var result = _mapper.Map<IList<StoreDTO>>(products);
+            var stores = await _unitOfWork.Stores.GetPagedList(requestParams);
+            var result = _mapper.Map<IList<StoreDTO>>(stores);
             return Ok(result);
         }
 
@@ -89,6 +89,28 @@ namespace API_Rest_Store_Management.Controller
 
             return NoContent();
         }
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 
+        public async Task<IActionResult> DeleteStore(int id)
+        {
+            if (id < 1)
+            {
+                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteStore)}");
+                return BadRequest();
+            }
+            var store = await _unitOfWork.Stores.Get(q => q.Id == id);
+            if (store == null)
+            {
+                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteStore)}");
+                return BadRequest("Submitted data is invalid");
+            }
+            await _unitOfWork.Stores.Delete(id);
+            await _unitOfWork.Save();
+            return NoContent();
+
+        }
     }
 }
